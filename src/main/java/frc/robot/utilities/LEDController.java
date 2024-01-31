cd923a9a9cf86186e55aa5c971bd8fd87b76611f
LamaOstritch <kaijvw@gmail.com>
2024-01-31 18:17:47 -0500

    added more functions to LEDS

--- src/main/java/frc/robot/utilities/LEDController.java
+++ src/main/java/frc/robot/utilities/LEDController.java
@@ -1,15 +1,21 @@
 package frc.robot.Utilities;
 
 import com.ctre.phoenix.led.CANdle;
+import com.ctre.phoenix.led.CANdleConfiguration;
+import com.ctre.phoenix.led.CANdle.LEDStripType;
+
 import frc.robot.Constants.Constants;
 
 public class LEDController {
+
     private static LEDController _instance;
     private CANdle candle;
+    private CANdleConfiguration config;
 
     public LEDController()
     {
         candle = new CANdle(Constants.kCANdleId);
+        config = new CANdleConfiguration();
     }
 
     public static LEDController getInstance()
@@ -19,5 +25,23 @@ public class LEDController {
             _instance = new LEDController();
         }
         return _instance;
+    }
+    /**
+     * sets the brightness of the strip
+     * @param brightness a number from 0 to 1 enter a precentage
+     */
+    public void setBrightness(double brightness)
+    {
+        config.brightnessScalar = brightness;
+    }
+
+    public void setLEDs(int red, int green, int blue)
+    {
+        candle.setLEDs(red, green, blue);
+    }
+
+    public void off()
+    {
+        config.brightnessScalar = 0;
     }
 }
