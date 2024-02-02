63f2eb947a5f3f2493f8718122d5514e9ec365fa
LamaOstritch <kaijvw@gmail.com>
2024-02-02 18:49:30 -0500

    added more features for the motors

--- src/main/java/frc/robot/Subsystems/Jukebox.java
+++ src/main/java/frc/robot/Subsystems/Jukebox.java
@@ -1,6 +1,9 @@
 package frc.robot.Subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
 import frc.robot.Constants.Constants;
     
 public class Jukebox {
@@ -26,6 +29,8 @@ public class Jukebox {
     {
         _elevatorL = new CANSparkMax(Constants.kLElevatorId, MotorType.kBrushless);
         _elevatorR = new CANSparkMax(Constants.kLElevatorId, MotorType.kBrushless);
+        _elevatorR.follow(_elevatorL);
+        _elevatorL.setIdleMode(IdleMode.kBrake);
     }
 
     public static Jukebox getInstance()
