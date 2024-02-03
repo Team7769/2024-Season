332a99b869ad11d1247aa7dd22c5b64e6e011bb3
Zee Pearson <57326432+babayaga88@users.noreply.github.com>
2024-02-02 20:08:27 -0500

    dd

--- src/main/java/frc/robot/Subsystems/ElevatorState.java
+++ src/main/java/frc/robot/Subsystems/ElevatorState.java
@@ -2,13 +2,6 @@ package frc.robot.Subsystems;
 
     public enum ElevatorState
     {
-        IDK,
-        RAMPUP,
-        SHOOT,
         RESET,
-        DUMPAMP,
-        SETUPFORAMP,
-        EXTEND,
-        CLIMB,
-        HOLD
+        HOLD_POSITION,
     }
--- src/main/java/frc/robot/Subsystems/Jukebox.java
+++ src/main/java/frc/robot/Subsystems/Jukebox.java
@@ -102,26 +102,13 @@ public class Jukebox {
         }
     }
 
-    private void down(){}
+    private void setManualElevatorDown(){}
 
-    private void up(){}
+    private void setManualElvatorUp(){}
 
     private void setSetpoint(double position)
     {
         _goal = new TrapezoidProfile.State(position, 0);
-
-        switch (currentState) {
-            case RESET:
-                break;
-            case HOLD:
-                break;
-            case DUMPAMP:
-                break;
-            case SETUPFORAMP:
-                break;
-            default:
-                break;
-        }
     }
 
     public boolean isItAtSetpoint()
@@ -149,49 +136,11 @@ public class Jukebox {
 
     private void handleCurrentState()
     {
-        switch (currentState) {
-            case IDK:
-                break;
-            case RAMPUP:
-                break;
-            case SHOOT:
-                break;
-            case RESET:
-                break;
-            case DUMPAMP:
-                break;
-            case SETUPFORAMP:
-                break;
-            case EXTEND:
-                break;
-            case CLIMB:
-                break;
-            default:
-                break;
-        }
     }
 
-    public void setState(ElevatorState e)
-    {
-        switch (e) {
-            case IDK:
-                break;
-            case RAMPUP:
-                break;
-            case SHOOT:
-                break;
-            case RESET:
-                break;
-            case DUMPAMP:
-                break;
-            case SETUPFORAMP:
-                break;
-            case EXTEND:
-                break;
-            case CLIMB:
-                break;
-        }
-    }
+    public void setState(ElevatorState e){}
 
+    public void SetElevatorSetPoint(double p)
+    {}
     
 }
