0f03ffbb4ccda06421ca005a077c81ebe0100114
Zee Pearson <57326432+babayaga88@users.noreply.github.com>
2024-02-02 19:02:56 -0500

    f

--- src/main/java/frc/robot/Subsystems/Jukebox.java
+++ src/main/java/frc/robot/Subsystems/Jukebox.java
@@ -1,13 +1,32 @@
 package frc.robot.Subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxPIDController;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import frc.robot.Constants.Constants;
     
 public class Jukebox {
-    
+
+    private static Jukebox _instance;
+    private CANSparkMax _elevatorL;
+    private CANSparkMax _elevatorR;
+    private ElevatorState currentState = ElevatorState.IDK;
+
+    private final double kSmartMotionP = 0;
+    private final double kSmartMotionI = 0;
+    private final double kSmartMotionD = 0;
+    private final double kSmartMotionFF = 0;
+    private final double kSmartMotionIZ = 0;
+    private final double kSmartMotionMaxOutput = 0;
+    private final double kSmartMotionMinOutput = 0;
+    private final double kSmartMotionMaxVel = 225;
+    private final double kSmartMotionMaxAccel = 150;
+    private final double kAllowedError = 3;
+
+
+
 
     
 
@@ -17,6 +36,9 @@ public class Jukebox {
         _elevatorR = new CANSparkMax(Constants.kLElevatorId, MotorType.kBrushless);
         _elevatorR.follow(_elevatorL);
         _elevatorL.setIdleMode(IdleMode.kBrake);
+
+        currentState = ElevatorState.IDK;
+        
     }
 
     public static Jukebox getInstance()
@@ -32,7 +54,25 @@ public class Jukebox {
     // @Override
     // public void logTelemetry(){}
 
-    private void switchCurrentState(){}
+    private void switchCurrentState()
+    {
+        switch (currentState) {
+            case IDK:
+                break;
+            case RAMPUP:
+                break;
+            case SHOOT:
+                break;
+            case RESET:
+                break;
+            case DUMPAMP:
+                break;
+            case SETUPFORAMP:
+                break;
+            case EXTEND:
+                break;
+        }
+    }
 
     public void setState(ElevatorState e)
     {
