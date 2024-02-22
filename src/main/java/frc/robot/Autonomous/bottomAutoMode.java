3232267581dc995fb04c7ce639a12ff12331ec94
babayaga88 <zeepearson412@gmail.com>
2024-02-22 15:51:03 -0500

    Finish Auto Bottom path. It's 12s long but thats the best I can do.

---
+++ src/main/java/frc/robot/Autonomous/bottomAutoMode.java
@@ -0,0 +1,86 @@
+package frc.robot.Autonomous;
+
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import frc.robot.Subsystems.Drivetrain;
+import frc.robot.Utilities.PathFollower;
+
+public class bottomAutoMode extends AutonomousMode{
+
+    private PathFollower _pathFollower;    
+    private Drivetrain _drivetrain;
+    private int _count;
+
+    public bottomAutoMode()
+    {
+        _drivetrain = Drivetrain.getInstance();
+        _pathFollower = new PathFollower("Auto Bottom 5");
+    }
+
+     @Override
+    public void execute()
+    {
+        switch (_count) {
+            case 0:
+                _pathFollower.startNextPath(new ChassisSpeeds(), _drivetrain.getGyroRotationWithOffset());
+                nextStep();
+                break;
+            case 1:
+                _drivetrain.drive(_pathFollower.getPathTarget(_drivetrain.getPose()));
+                if (_pathFollower.isPathFinished()){
+                    _drivetrain.drive(new ChassisSpeeds());
+                    _pathFollower.startNextPath(new ChassisSpeeds(), _drivetrain.getGyroRotationWithOffset());
+                    nextStep();
+                }
+                break;
+            case 2:
+                _drivetrain.drive(_pathFollower.getPathTarget(_drivetrain.getPose()));
+                if (_pathFollower.isPathFinished()){
+                    _drivetrain.drive(new ChassisSpeeds());
+                    _pathFollower.startNextPath(new ChassisSpeeds(), _drivetrain.getGyroRotationWithOffset());
+                    nextStep();
+                }
+                break;
+            case 3:
+                _drivetrain.drive(_pathFollower.getPathTarget(_drivetrain.getPose()));
+                if (_pathFollower.isPathFinished()){
+                    _drivetrain.drive(new ChassisSpeeds());
+                    _pathFollower.startNextPath(new ChassisSpeeds(), _drivetrain.getGyroRotationWithOffset());
+                    nextStep();
+                }
+                break;
+            case 4:
+                _drivetrain.drive(_pathFollower.getPathTarget(_drivetrain.getPose()));
+                if (_pathFollower.isPathFinished()){
+                    _drivetrain.drive(new ChassisSpeeds());
+                    _pathFollower.startNextPath(new ChassisSpeeds(), _drivetrain.getGyroRotationWithOffset());
+                    nextStep();
+                }
+                break;
+            case 5:
+                _drivetrain.drive(_pathFollower.getPathTarget(_drivetrain.getPose()));
+                if (_pathFollower.isPathFinished()){
+                    _drivetrain.drive(new ChassisSpeeds());
+                    _pathFollower.startNextPath(new ChassisSpeeds(), _drivetrain.getGyroRotationWithOffset());
+                    nextStep();
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void abort(){}
+
+    @Override
+    public boolean isComplete(){
+        return _count >= 5;
+    }
+    @Override
+    public void initialize(){
+        var startingPose = _pathFollower.getStartingPose();
+        _drivetrain.setStartingPose(startingPose);
+        _count = 0;
+    }
+    private void nextStep() {
+        _count++;
+    }
+}
