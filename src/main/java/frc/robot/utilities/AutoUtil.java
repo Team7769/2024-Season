15790b2006ccc5f3d489e3d35555c847dc6d2c3a
Isaac <54126919+Aizakkuno@users.noreply.github.com>
2024-02-24 10:00:03 -0500

    added auton

--- src/main/java/frc/robot/utilities/AutoUtil.java
+++ src/main/java/frc/robot/utilities/AutoUtil.java
@@ -28,7 +28,9 @@ public class AutoUtil {
             case(MID_B_3_4_5):
                 return new midB345();
             case(FIVE_NOTE):
-                return new FiveNote();            
+                return new FiveNote(); 
+            case (TOP_A_1_2_3_INDEX):
+                return new TOP_A_1_2_3();       
         }
         
         return null;
@@ -49,6 +51,7 @@ public class AutoUtil {
         _autoChooser.addOption("Mid B 4 5 (4)", MID_B_4_5);
         _autoChooser.addOption("Mid B 3 4 5 (5)", MID_B_3_4_5);
         _autoChooser.addOption("Five Note", FIVE_NOTE);
+        _autoChooser.addOption("Top A 1 2 3", TOP_A_1_2_3_INDEX);
     }
 
     // list of all auto modes 
@@ -60,4 +63,5 @@ public class AutoUtil {
     public static final int MID_B_4_5 = 4;
     public static final int MID_B_3_4_5 = 5;
     public static final int FIVE_NOTE = 6;
+    public static final int TOP_A_1_2_3_INDEX = 7;
 }
